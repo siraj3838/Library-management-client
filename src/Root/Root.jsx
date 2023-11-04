2bfb1c66e924248e147ea3557e3fee5849973b73
Sirajul Islam <ayasabegum134@gmail.com>
2023-11-05 02:11:13 +0600

    I Create My active Footer Section

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar";
+import Footer from "../Pages/Footer";
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
