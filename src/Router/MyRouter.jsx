26e61be7cfe8debaf4a270cfa75ac800a56932af
Sirajul Islam <ayasabegum134@gmail.com>
2024-02-26 22:20:59 +0600

    i create description details page

--- src/Router/MyRouter.jsx
+++ src/Router/MyRouter.jsx
@@ -58,7 +58,7 @@ const MyRouter = createBrowserRouter([
             {
                 path: '/descriptionDetails/:id',
                 element: <BookDescriptionDetails></BookDescriptionDetails>
-            }
+            },
             
         ]
     }
